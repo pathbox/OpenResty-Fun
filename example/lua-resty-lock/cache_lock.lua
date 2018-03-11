14d3a0755433d237f35aa78e17d60221f26149db
pathbox <1064569731@qq.com>
2018-03-11 16:02:05 +0800

    lua-resty-lock 解决缓存(雪崩)失效问题

--- example/lua-resty-lock/cache_lock.lua
+++ example/lua-resty-lock/cache_lock.lua
@@ -7,7 +7,7 @@ local resty_lock = require "resty.lock"
 local cache = ngx.shared.my_cache 
 
 -- step 1:
-local val, err = cache:get(key)
+local val, err = cache:get(key)  -- 从缓存中获取值
 if val then 
     ngx.say("result: ", val)
     return
@@ -17,14 +17,14 @@ if err then
     return fail("failed to get key from shm: ", err)
 end
 
--- cache miss!
+-- cache miss! 
 -- step 2:
-local lock, err = resty_lock:new("my_locks")  -- lua_shared_dict my_locks 100k;
+local lock, err = resty_lock:new("my_locks")  -- 新建 resty_lock 锁
 if not lock then 
     return fail("failed to create lock: ", err)
 end
 
-local elapsed, err = lock:lock(key)
+local elapsed, err = lock:lock(key) -- 对 key 加锁
 if not elapsed then 
     return fail("failed to acquire the lock: ", err)
 end
@@ -36,8 +36,8 @@ end
 -- so we check it here again:
 val, err = cache:get(key)
 if val then 
-    local ok, err = lock:unlock()
-    if not then 
+    local ok, err = lock:unlock()  -- 解锁
+    if not ok then 
         return fail("failed to unlock: ", err)
     end
 
@@ -46,9 +46,9 @@ if val then
 end
 
 --step 4:
-local val = fetch_redis(key)
+local val = fetch_redis(key)  -- 从(数据库)redis 中查找key
   if not val then 
-    local, ok, err = lock:unlock()
+    local ok, err = lock:unlock() -- 没有获取到值，解锁
     if not ok then
         return fail("failed to unlock: ", err)
     end
@@ -61,9 +61,9 @@ local val = fetch_redis(key)
 end
 
 -- update the shm cache with the newly fetched value
-local ok, err = cache:set(key, val, 1)
+local ok, err = cache:set(key, val, 1) -- 从redis中获取到val，对cache 进行更新
 if not ok then
-    local ok, err = lock:unlock()
+    local ok, err = lock:unlock() -- 更新出错 解锁
     if not ok then
         return fail("failed to unlock: ", err)
     end
@@ -71,12 +71,16 @@ if not ok then
     return fail("failed to update shm cache: ", err)
 end
 
-local ok, err = lock:unlock()
+local ok, err = lock:unlock() -- 最后 解锁
 if not ok then
     return fail("failed to unlock: ", err)
 end
 
+-- 流程注释： 从cache中取值，取到值返回，没有取到，使用resty_lock 加锁， 从redis中重新获取值，获取成功，更新cache中的值，最后解锁
+-- 使用resty_lock 保证在更新缓存的时候，只有一个操作在进行，更新完成之后，cache中就有值了，之后的访问就会从cache中获取，而不会再进行数据库查询
+-- 这是一种处理 缓存（雪崩）失效的方法，对缓存失效后的第一次更新缓存操作加锁，更新缓存，缓存重新拥有值，之后的查询将从缓存中获取，而不会查询数据库
 
-
-
+-- 简单来说就是缓存重建时，当多个客户端对同一个缓存数据发起请求时，会在客户端采用加锁等待的方式，对同一个Cache的重建需要获取到相应的锁才行，
+-- 只有一个客户端能拿到锁，并且只有拿到锁的客户端才能访问数据库重建缓存，其它的客户端都需要等待这个拿到锁的客户端重建好缓存后直接读缓存，
+-- 其结果是对同一个缓存数据，只进行一次数据库重建访问。但是如果访问分散比较严重，还是会瞬间对数据库造成非常大的压力。
 
